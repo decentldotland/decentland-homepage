ed06db7c2f8d92c121c4eb7a732eb2f481aa7e4b
Max Knivets <knivets.m@gmail.com>
2022-08-24 12:11:18 -0400

    feat: ark minting site live banner

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ import LogoBar from './components/logo-bar'
 import Callout from './components/callout'
 
 function App() {
+  const week = new Date("Thu Sep 01 2022 12:00:00 GMT-0700 (Pacific Daylight Time)");
+
   return (
     <div>
       <div className="w-full">
         <Navbar />
+        {week > new Date() && (
+          <div className="bg-gray-900">
+            <div className="py-4 w-full bg-gradient-to-r from-purple-900/30 to-green-600/30 text-white text-bold text-2xl text-center">Our first official mint is now live! Get yourself a settler pass <a href="https://mint.decent.land" className="text-green-400 hover:text-green-400/40">here</a>!</div>
+          </div>
+        )}
         <Hero />
       </div>
       <Features />
